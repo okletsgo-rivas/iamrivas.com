18dcf929d066babc97f3de46c23f04dee12564be
John Rivas <jrivas@cranemorley.com>
2022-05-13 18:53:17 -0700

    3D animation

--- src/lib/Background.svelte
+++ src/lib/Background.svelte
@@ -4,16 +4,25 @@
 
 <script lang="ts">
 	import { onMount } from 'svelte';
+	import { tweened } from 'svelte/motion';
+	import { cubicOut } from 'svelte/easing';
+
 	import * as THREE from 'three';
 	import * as SC from 'svelte-cubed';
 	import type { Position } from 'svelte-cubed/types/common';
 	import type { Texture } from 'three';
 
-	let spin = 0;
+	let spinY = 0;
+	let spinZ = 0;
+	let coin = new THREE.TextureLoader();
+
+	const progress = tweened(0, {
+		duration: 500,
+		easing: cubicOut
+	});
 
 	SC.onFrame(() => {
-		// if (!intersects)
-		spin += 0.0025;
+		spinY = (spinY + spinSpeed) % ((360 * Math.PI) / 180);
 	});
 	let blockArr: any = [0, 1]
 		.map((i) =>
@@ -21,15 +30,14 @@
 				[0, 1].map((k) => ({
 					path: [i, j, k],
 					position: [0, 0, 0] as Position,
-					color: (i + j + k) % 2 ? 0xffffff : 0x222222,
-					tween: null
+					color: (i + j + k) % 2 ? 0xffffff : 0x222222
 				}))
 			)
 		)
 		.flat(2);
 
 	let planeTexture: Texture;
-	let cameraPostion: number[] = [1, 1, 3];
+	let cameraPostion: number[] = [1, 1.5, 3];
 	// let projector = new THREE.Projector();
 	let mouse = { x: 0, y: 0, z: 1 };
 	let intersects: boolean;
@@ -41,16 +49,18 @@
 	const mesh = new THREE.Mesh(box);
 	// box.translate(0.5, 0.25, 0);
 	mesh.position.set(0.5, 0.25, 0);
-	$: mesh.rotation.y = (spin * Math.PI) / 180;
 
-	$: blockArr.map((block: any, i: number) => {
-		blockArr[i].position = block.path.map((val: number) => {
-			const factor = intersects ? 0.5 : 0.25;
-			const posNeg = val ? 1 : -1;
-			const add = factor * posNeg;
-			return add;
+	// $: mesh.rotation.y = (spin * Math.PI) / 180;
+	$: scale = $progress * -0.45 + 0.5;
+	$: spinZ = intersects ? spinY / 2 : 0;
+	$: $progress = intersects ? 1 : 0;
+	$: spinSpeed = (intersects ? 5 : 0.5) * (Math.PI / 180);
+	$: {
+		const factor = $progress * 0.25 + 0.25;
+		blockArr.map((block: any, i: number) => {
+			blockArr[i].position = block.path.map((val: number) => factor * (val ? 1 : -1));
 		});
-	});
+	}
 
 	function onMouseMove(e: MouseEvent) {
 		e.preventDefault();
@@ -75,6 +85,7 @@
 </script>
 
 <svelte:body on:mousemove={onMouseMove} />
+
 <SC.Canvas
 	antialias
 	background={new THREE.Color(0xd9e5e7)}
@@ -93,6 +104,19 @@
 		/>
 	</SC.Group>
 
+	<SC.Group position={[0.5, 0.25, 0]}>
+		<SC.Mesh
+			geometry={new THREE.PlaneGeometry(1, 1)}
+			material={new THREE.MeshBasicMaterial({
+				transparent: true,
+				map: coin.load('profile.jpg')
+			})}
+			scale={0.5}
+			rotation={[0, 0, 0]}
+			receiveShadow
+		/>
+	</SC.Group>
+
 	<!-- Test Hitbox
 	<SC.Mesh
 		geometry={box}
@@ -101,7 +125,7 @@
 		rotation={[0, spin, 0]}
 	/> -->
 
-	<SC.Group position={[0.5, 0.25, 0]} rotation={[0, spin, 0]}>
+	<SC.Group position={[0.5, 0.4, 0]} rotation={[0, spinY, spinZ]}>
 		{#each blockArr as block}
 			<SC.Mesh
 				geometry={new THREE.BoxGeometry()}
@@ -109,14 +133,13 @@
 					color: block.color
 				})}
 				position={block.position}
-				scale={[0.5, 0.5, 0.5]}
+				scale={[scale, scale, scale]}
 				castShadow
 			/>
 		{/each}
 	</SC.Group>
 
 	<SC.PerspectiveCamera bind:position={cameraPostion} />
-	<SC.OrbitControls maxPolarAngle={Math.PI * 0.51} />
 	<SC.AmbientLight intensity={0.9} />
 	<SC.DirectionalLight intensity={0.1} position={[4, 3, 1]} shadow={{ mapSize: [2048, 2048] }} />
 </SC.Canvas>
