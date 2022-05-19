78f4b867af02cb90f53f7b1835adb8399ba7c307
John Rivas <jrivas@cranemorley.com>
2022-05-19 11:50:52 -0700

    Link updates

--- src/lib/header/Header.svelte
+++ src/lib/header/Header.svelte
@@ -1,7 +1,16 @@
 <script lang="ts">
 	// import { page } from '$app/stores';
 	import Fa from 'svelte-fa';
-	import { faGithub, faLinkedin, faPinterest } from '@fortawesome/free-brands-svg-icons';
+	import {
+		faGithub,
+		faLinkedin,
+		faPinterest,
+		faReact,
+		faJs,
+		faVuejs,
+		faAngular,
+		faDrupal
+	} from '@fortawesome/free-brands-svg-icons';
 
 	import logo from './logo.svg';
 
@@ -9,19 +18,67 @@
 		{
 			title: 'View with Framework',
 			links: [
-				{ href: 'https://js.iamrivas.com/', title: 'Vanilla JS', enabled: false },
-				{ href: 'https://react.iamrivas.com/', title: 'React', enabled: true },
-				{ href: 'http://vue.iamrivas.com/', title: 'Vue', enabled: true },
-				{ href: 'https://svelte.iamrivas.com/', title: 'Svelte', enabled: false },
-				{ href: 'https://ng.iamrivas.com/', title: 'Angular 10 (WIP)', enabled: true },
-				{ href: 'https://d9.iamrivas.com/', title: 'Drupal (WIP)', enabled: true }
+				{
+					href: 'https://svelte.iamrivas.com/',
+					title: 'Svelte (WIP)',
+					icon: faJs,
+					iconColor: '#F5B21B',
+					enabled: false
+				},
+				{
+					href: 'https://react.iamrivas.com/',
+					title: 'React',
+					icon: faReact,
+					iconColor: '#60D8FB',
+					enabled: true
+				},
+				{
+					href: 'http://vue.iamrivas.com/',
+					title: 'Vue',
+					icon: faVuejs,
+					iconColor: '#40B783',
+					enabled: true
+				},
+				{
+					href: 'https://ng.iamrivas.com/',
+					title: 'Angular 10 (WIP)',
+					icon: faAngular,
+					iconColor: '#D82C31',
+					enabled: true
+				},
+				{
+					href: 'https://js.iamrivas.com/',
+					title: 'Vanilla JS (WIP)',
+					icon: faJs,
+					iconColor: '#F5B21B',
+					enabled: false
+				},
+				{
+					href: 'https://d9.iamrivas.com/',
+					title: 'Drupal (WIP)',
+					icon: faDrupal,
+					iconColor: '#098ACE',
+					enabled: true
+				}
 			]
 		},
 		{
 			title: 'View with SSR stack',
 			links: [
-				{ href: 'http://sveltekit.iamrivas.com/', title: 'SveltKit', enabled: false },
-				{ href: 'http://gatsby.iamrivas.com/', title: 'React / Drupal / Gatsby', enabled: true }
+				{
+					href: 'http://sveltekit.iamrivas.com/',
+					title: 'SveltKit',
+					icon: faJs,
+					iconColor: '#F5B21B',
+					enabled: false
+				},
+				{
+					href: 'http://gatsby.iamrivas.com/',
+					title: 'React/Drupal/Gatsby (WIP)',
+					icon: faJs,
+					iconColor: '#F5B21B',
+					enabled: true
+				}
 			]
 		}
 	];
@@ -61,10 +118,11 @@
 	<nav>
 		<h1 style="margin-top:0.5rem">Portfolio</h1>
 		{#each portLinks as group}
-			<strong>{group.title}</strong>
-			<ul style="margin:0.5rem 0 2rem 0">
+			<strong style="color:#666">{group.title}</strong>
+			<ul style="margin:0.5rem 0 2rem 0;padding:0;list-style:none">
 				{#each group.links as link}
-					<li>
+					<li style="margin-bottom:0.5rem">
+						<Fa icon={link.icon} size="lg" fw color={link.iconColor} />
 						<a href={link.href} target="_blank" class:disable={!link.enabled}>{link.title}</a>
 					</li>
 				{/each}
@@ -89,8 +147,8 @@
 	}
 	nav {
 		display: inline-block;
-		padding: 1rem;
-		background: rgba(255, 255, 255, 0.5);
+		padding: 1.5rem;
+		background: rgba(255, 255, 255, 0.4);
 		pointer-events: all;
 	}
 
